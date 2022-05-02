af832503bf5edf05cf17d5152a9aa4938af59e44
tsehelnykalona <tsehelnykalona@outlook.com>
2022-05-02 05:55:29 +0300

    Add

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,5 @@ import stakingABI from "../abis/Staking.json";
 import cashpTokenABI from "../abis/CashP.json";
 export const stakingAbi = stakingABI.abi;
 export const cashpAbi = cashpTokenABI.abi;
-export const stakingContractAddress = "0x9438900F9122eb122e610C81093cBbcB286B54f0";
-export const cashpTokenContractAddress = "0x011Ab0915231EE7425B253CD4357C5Bf1B68107C";
+export const stakingContractAddress = "0xA5A97a70637d24750C6c50048f7B7fA94dd7dAdd";
+export const cashpTokenContractAddress = "0x3f7bAac90aF49620b86659F19a02B01f834E714d";
